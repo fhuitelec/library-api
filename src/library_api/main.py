a1cee31781f95f4bb6ed254ee5c99ec170c40f05
Fabien Huitelec <fabien@huitelec.fr>
2025-10-07 17:08:00 +0200

    feat(http): bootstrap FastAPI HTTP server

--- src/library_api/main.py
+++ src/library_api/main.py
@@ -1,10 +1,27 @@
 """Root module of library-api package."""
 
+from typing import Union
 
-def main() -> None:
-    """Print a greeting message."""
-    print("Hello from library-api!")
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root() -> dict[str, str]:
+    """Return a greeting message."""
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(
+    item_id: int, q: Union[str, None] = None
+) -> dict[str, Union[str, int, None]]:
+    """Read items."""
+    return {"item_id": item_id, "q": q}
+
+
+def server() -> None:
+    """Run the ASGI server."""
+    uvicorn.run(app, host="0.0.0.0", port=8000)
