cfa4663a815a80e273b032a1b1848749bf5f3fbb
Fabien Huitelec <fabien@huitelec.fr>
2025-10-14 18:33:52 +0200

    docs|wip(model): few hints on the Book model

--- src/library_api/main.py
+++ src/library_api/main.py
@@ -1,9 +1,12 @@
 """Root module of library-api package."""
 
+import datetime
+import uuid
 from typing import Union
 
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -25,3 +28,22 @@ def read_item(
 def server() -> None:
     """Run the ASGI server."""
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+class Book(BaseModel):
+    """Book model."""
+
+    id: uuid.UUID
+    human_id: str  # EDITOR-TITLE-ID, eg. ALI-FEELGOOD-001
+    issue: int
+    isbn: str
+    title: str
+    title: str
+    author: str
+    publisher: str
+    publish_date: datetime.datetime
+
+
+# uuid.uuid7() -> passer à Python 3.14 -> https://docs.python.org/3/library/uuid.html#uuid.uuid7
+# Primary key indexation en UUIDv7
+# Constraint on human_id + copy: https://stackoverflow.com/a/14221810
