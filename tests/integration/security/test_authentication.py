367543b8ab28c5fe575f9bd7945c465056b224f4
Fabien Huitelec <fabien@huitelec.fr>
2025-10-20 15:19:22 +0200

    test(auth): create test dedicated endpoint

--- tests/integration/security/test_authentication.py
+++ tests/integration/security/test_authentication.py
@@ -1,15 +1,25 @@
 """Integration tests for authentication."""
+from typing import Annotated
 
 import pytest
+from fastapi import Depends
 from fastapi.testclient import TestClient
 
-from library_api.api.security import Permission
+from library_api.api.kernel import app
+from library_api.api.security import Permission, JWT
+from library_api.api.security.authentication import authentication
 from tests.integration.conftest import craft_jwt, JWK
+
+
+@app.get("/test/authn")
+async def authentication_introspection(jwt: Annotated[JWT, Depends(authentication)]) -> JWT:
+    """Return the JWT payload content."""
+    return jwt
 
 
 def test_no_authorization_header(client: TestClient) -> None:
     """Test an unauthenticated request returns an HTTP/401."""
-    response = client.get("/auth/introspection")
+    response = client.get("/test/authn")
     assert response.status_code == 401
     assert response.json()["detail"] == "Not authenticated"
 
@@ -23,14 +33,14 @@ def test_no_authorization_header(client: TestClient) -> None:
 )
 def test_malformed_jwt(client: TestClient, jwk: JWK, jwt: str) -> None:
     """Test an unauthenticated request returns an HTTP/401."""
-    response = client.get("/auth/introspection", headers={"Authorization": "Bearer malformed.jwt.token"})
+    response = client.get("/test/authn", headers={"Authorization": "Bearer malformed.jwt.token"})
     assert response.status_code == 401
 
 
 def test_valid_jwt(client: TestClient, jwk: JWK) -> None:
     """Test an unauthenticated request returns an HTTP/401."""
     jwt, raw_jwt = craft_jwt(jwk=jwk, permissions={Permission.BOOK_READ})
-    response = client.get("/auth/introspection", headers={"Authorization": f"Bearer {raw_jwt}"})
+    response = client.get("/test/authn", headers={"Authorization": f"Bearer {raw_jwt}"})
     assert response.status_code == 200
 
     body = response.json()
