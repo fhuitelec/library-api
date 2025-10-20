6d700951741565993bb6fe23a941521242f1a53b
Fabien Huitelec <fabien@huitelec.fr>
2025-10-20 17:06:02 +0200

    refactor(auth): simplify the error model

--- src/library_api/api/security/exceptions.py
+++ src/library_api/api/security/exceptions.py
@@ -1,7 +1,6 @@
 """Exceptions for the security package."""
 
 from http import HTTPStatus
-from typing import ClassVar
 
 from jwt import PyJWTError
 from pydantic import BaseModel, Field
@@ -13,32 +12,17 @@ from starlette.responses import Response, JSONResponse
 class AuthenticationError(BaseModel):
     """Error during the authentication process."""
 
-    UNAUTHENTICATED_RESOLUTION: ClassVar[str] = "You must add a bearer 'Authorization' header with a valid JWT token."
-
-    detail: str = Field(examples=["Cannot decode JWT"], description="Details about the authentication issue")
-    resolution: str | None = Field(
-        default=None, examples=["Add X-Example header"], description="Hints on how to resolve the issue"
-    )
-
-    @classmethod
-    def jwt_error(cls, exc: Exception, resolution: str | None = None) -> "AuthenticationError":
-        """Create an AuthenticationResponse from an exception."""
-        return AuthenticationError(detail=str(exc), resolution=resolution or cls.UNAUTHENTICATED_RESOLUTION)
-
-    @classmethod
-    def unauthenticated(cls, exc: HTTPException) -> "AuthenticationError":
-        """Create an AuthenticationResponse from an exception."""
-        return AuthenticationError(detail=exc.detail, resolution=cls.UNAUTHENTICATED_RESOLUTION)
+    detail: str | None = Field(examples=["Cannot decode JWT"], description="Details about the authentication issue")
 
 
 async def jwt_exception_handler(_: Request, exc: PyJWTError) -> Response:
     """Handle JWT-related exceptions."""
-    return JSONResponse(AuthenticationError.jwt_error(exc).model_dump(), status_code=HTTPStatus.UNAUTHORIZED)
+    return JSONResponse(AuthenticationError(detail=str(exc)).model_dump(), status_code=HTTPStatus.UNAUTHORIZED)
 
 
 async def unauthorized_exception_handler(_: Request, exc: HTTPException) -> Response:
     """Handle FastAPI native authentication exceptions."""
     if exc.status_code == HTTPStatus.UNAUTHORIZED:
-        return JSONResponse(AuthenticationError.unauthenticated(exc).model_dump(), status_code=HTTPStatus.UNAUTHORIZED)
+        return JSONResponse(AuthenticationError(detail=exc.detail).model_dump(), status_code=HTTPStatus.UNAUTHORIZED)
 
     raise RuntimeError(f"Unhandled exception: {exc}")
