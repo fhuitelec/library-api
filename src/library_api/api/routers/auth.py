dec6a29c40c7caf28a87f0893e84661a951583ef
Fabien Huitelec <fabien@huitelec.fr>
2025-10-25 17:06:18 +0200

    chore(auth): remove outdated comments

--- src/library_api/api/routers/auth.py
+++ src/library_api/api/routers/auth.py
@@ -12,39 +12,6 @@ router = APIRouter(
     tags=["authentication"],
 )
 
-######
-# 1. #
-######
-
-# @router.get("/introspection")
-# async def authentication_introspection(jwt: Annotated[str, Depends(oauth())]) -> str:
-#     """Return the JWT payload content."""
-#     return jwt
-
-
-######
-# 2. #
-######
-
-# @router.get("/introspection")
-# async def authentication_introspection(raw_jwt: Annotated[str, Depends(oauth())]) -> dict:
-#     """Return the JWT payload content."""
-#     import jwt as pyjwt
-#
-#     jwt = pyjwt.decode(
-#         jwt=raw_jwt,
-#         verify=False,
-#         audience="library-api",
-#         algorithms=["HS256"],
-#         options={"verify_signature": False},
-#     )
-#
-#     return jwt
-
-######
-# 3. #
-######
-
 
 @router.get("/introspection")
 async def authentication_introspection(jwt: Annotated[JWT, Depends(authentication)]) -> JWT:
